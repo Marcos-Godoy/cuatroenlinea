72b608fb52c8224ee733fee64b77d4c82608d5d1
Marcos Godoy <mjgodoy@gmail.com>
2021-03-30 16:54:24 -0300

    validacion de secuencia

--- prototipo.py
+++ prototipo.py
@@ -1,5 +1,4 @@
-secuencia = [1, 2, 3, 1]
-
+secuencia = [1, 2, 3, 1, 1, 6]
 
 def tableroVacio():
 	return [
@@ -12,24 +11,43 @@ def tableroVacio():
 	]
 
 def completarTableroEnOrden(secuencia, tablero):
-	for x in range(0, len(secuencia)):
-		if x % 2 == 0:
-			soltarFichaEnColumna(1, secuencia[x], tablero)
-		else:
-			soltarFichaEnColumna(2, secuencia[x], tablero)
+	ban = validarSecuencia(secuencia)
+	if ban != 1:
+		for x in range(0, len(secuencia)):
+			if x % 2 == 0:
+				soltarFichaEnColumna(1, secuencia[x], tablero)
+			else:
+				soltarFichaEnColumna(2, secuencia[x], tablero)
 	return tablero
 
+def validarSecuencia(secuencia):
+	ban = 0
+	for x in range(0, len(secuencia)):
+		if secuencia[x] > 7:
+			#print("Secuencia incorrecta!! Por favor vuelva a intentarlo.")
+			ban = 1
+			break
+	return ban
+
 def soltarFichaEnColumna(ficha, columna, tablero):
-	for fila in range(6, 0, -1):
-		if tablero[fila - 1][columna - 1] == 0:
-			tablero[fila - 1][columna - 1] = ficha
-			return
+	ban = validarSecuencia(secuencia)
+	if ban != 1:
+		for fila in range(6, 0, -1):
+			if tablero[fila - 1][columna - 1] == 0:
+				tablero[fila - 1][columna - 1] = ficha
+				return
+
 
 def dibujarTablero(tablero):
-	for fila in range(0, 6):
-		for columna in range(0, 7):
-			print(tablero[fila][columna], end = " ")
-		print('')
+	ban = validarSecuencia(secuencia)
+	if ban != 1:
+		for fila in range(0, 6):
+			for columna in range(0, 7):
+				print(tablero[fila][columna], end = " ")
+			print('')
+	else:
+		print("Secuencia incorrecta!! Por favor vuelva a intentarlo.")
+
 
 dibujarTablero(
 	completarTableroEnOrden(
